4363538102d43f426c7216c30a1503563cc99cdf
Yunojoe <joe.ford@yunomedia.com>
2019-06-03 17:19:35 +0100

    main commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+<h1>Content for the contact page</h1>
+@endsection
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+<h1> content for welcome page</h1>
+    
+@endsection
